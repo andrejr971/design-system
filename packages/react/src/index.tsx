57a1cf1e8fc4c0d25359448862fb8466f43d2c8c
andrejr971 <andrejunior179@gmail.com>
2022-10-11 19:18:53 -0300

    feat: add stitches

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,11 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$sm',
+})
+
+export function App() {
+  return <Button>sakjkdsjak</Button>
+}
