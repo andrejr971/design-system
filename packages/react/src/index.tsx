d3817875500421103133ebf8a06ec8931326b053
andrejr971 <andrejunior179@gmail.com>
2022-10-11 20:35:10 -0300

    feat: add storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,11 +1,32 @@
+import { ComponentProps } from 'react'
+
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
+  backgroundColor: '$ignite300',
   borderRadius: '$sm',
+
+  border: 0,
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    sizes: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    sizes: 'small',
+  },
 })
 
-export function App() {
-  return <Button>sakjkdsjak</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
