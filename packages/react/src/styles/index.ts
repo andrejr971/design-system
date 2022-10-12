e1bede9cb5c8ca9092f865d83092cdb8ad384a95
andrejr971 <andrejunior179@gmail.com>
2022-10-12 11:12:07 -0300

    fix: add changeset'

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@andrejr-dev/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
